c64feb7fdf45c116a744db660e93cd569922fb59
ezSan <ezequiel.sanchez.7391@gmail.com>
2023-05-23 16:57:10 -0300

    feat: landingPage OK

--- client/daily-manager/src/pages/components/MainLayout/MainLayout.js
+++ client/daily-manager/src/pages/components/MainLayout/MainLayout.js
@@ -3,16 +3,21 @@ import styles from "../../scss/globals.module.scss";
 import Header from "../header/Header";
 import GreenPanel from "../greenPanel/GreenPanel";
 import Footer from "../footer/Footer";
+import PrincipalComponent from "../principalComponent/PrincipalComponent";
 
 const { mainLayout } = styles;
 
 export default function MainLayout() {
   return (
     <div className={mainLayout}>
-      <Header/>
-      <GreenPanel/>
-      <Footer/>
-      
+      <Header />
+
+      <div style={{ display: "flex" }}>
+        <GreenPanel />
+        <PrincipalComponent />
+      </div>
+
+      <Footer />
     </div>
   );
 }
---
+++ client/daily-manager/src/pages/components/principalComponent/PrincipalComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+import LandingImg from "../../../../public/landingImg.png";
+import styles from "../../scss/globals.module.scss";
+
+const { ppalComponent, landingImg } = styles;
+
+export default function PrincipalComponent() {
+  return (
+    <div className={ppalComponent}>
+      <h3>Te ayudamos a manejar tu tiempo y prioridades</h3>
+      <Image src={LandingImg} alt='landingImg' className={landingImg} />
+    </div>
+  );
+}
